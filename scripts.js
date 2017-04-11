130fc50eaa17f6ca2ebc28fe29a47afc7006aef2
Cassia Almeida <lisa.shiphrah@gmail.com>
2017-04-11 10:25:29 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,129 +1,344 @@
-var mode = '';
-var series = new Array();
-var currentColor = '';
-var mistake = false;
-var playerTurn = false;
+var machinePlay = [];
+var userPlay = [];
+var rand = 1;
+var count = 0;
+var counter = 0;
+var userCount = 0;
+var n = 0;
 
-$(document).ready(function(){
+var yellowAudio = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound3.mp3');
+var blueAudio = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound4.mp3');
+var redAudio = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound2.mp3');
+var greenAudio = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound1.mp3');
 
-	$('.sides').click(function(){
-		if(playerTurn) {
-			playerMovement($(this).attr('id'));
-			setTimeout(function() { generateMovement();},1900);
-		}
-	});
+function yellowClick() {
+	yellowLight();
+	userPlay.push(3);
+	userMovement();	
+}
+
+function blueClick() {
+	blueLight();
+	userPlay.push(4);
+	userMovement();	
+}
+
+function redClick() {
+	redLight();
+	userPlay.push(2);
+	userMovement();	
+}
+
+function greenClick(){
+	greenLight();
+	userPlay.push(1);
+	userMovement();
+}
+
+// Functions for each button:
+function greenLight() {
+	turnOffLights();
+	$('#green').toggleClass("greenGlow");
+	greenAudio.play();
+}
+function redLight() {
+	turnOffLights()
+	$('#red').toggleClass("redGlow");
+	redAudio.play();
+}
+function yellowLight() {
+	turnOffLights()	
+	$('#yellow').toggleClass("yellowGlow");
+	yellowAudio.play();
+}
+function blueLight() {
+	turnOffLights()
+	$('#blue').toggleClass("blueGlow");
+	blueAudio.play();
+}
+
+// Reset function for all buttons:
+function turnOffLights() {
+	$("#green").removeClass("greenGlow");
+	$("#red").removeClass("redGlow");
+	$("#yellow").removeClass("yellowGlow");
+	$("#blue").removeClass("blueGlow");			
+}
+
+// Game start function:
+function startGame() {
+	cleansCounters();
+	$('startButton').hide();
+	$('#score').val('1');
+	machineMovement();
+}
+
+var z = -1;
+var w = -1;
+
+// Input switches control On/Off State and Strict Mode:
+$('#switchRight').change(function(){
+	z *= -1;
+});
+
+$('#switchLeft').change(function(){
+
+	w *= -1;
+
+	if ( w > 0 ) {
+		$("#startButton").css("color", "black");
+	}
+
+	if ( w < 0 ) {
+
+		n = 0;
+		machinePlay = [];
+		userPlay = [];
+		count = 0;
+		counter = 0;
+		userCount = 0;
+		turnOffLights();
+		$('#score').val('0');	
+
+		$("#startButton").css("color", "rgb(200,200,200");
+
+		$("#green").removeClass("greenLoser");
+		$("#red").removeClass("redLoser");
+		$("#yellow").removeClass("yellowLoser");
+		$("#blue").removeClass("blueLoser");
+
+		$("#green").removeClass("greenWinner");
+		$("#red").removeClass("redWinner");
+		$("#yellow").removeClass("yellowWinner");
+		$("#blue").removeClass("blueWinner");
+
+	}
 
 });
 
-/*
-*	Function responsible for starting the game 
-*/
-function startGame() {
-	$('#startButton').hide();
-	generateMovement();
+// Computer plays function:
+function machineMovement() {
+	var x = 1;
+	function clear() {
+		clearInterval(machineMovement);
+	}
+
+	if ( count > 0 ) {
+		x = 0;
+	}
+
+	var machineMovement = setInterval(function() {
+
+			turnOffLights();
+			generateRandomPlay();
+			
+			if (rand === 1) {
+				greenLight();
+			}
+			else if (rand === 2) {
+				redLight();
+			}
+			else if (rand === 3) {
+				yellowLight();	
+			}
+			else if (rand === 4) {
+				blueLight();		
+			}
+
+			count++;
+
+			// Resets buttons after machine plays final round, so the user can continue:
+			if (count === counter + 1) {
+				clear();
+				setTimeout(function() {
+					turnOffLights();
+				}, 500);
+			}
+
+	}, 750 * x); // Pause between iterations of button press functions
+
+};
+
+// Generate a random number between 1 and 4 for each round:
+function generateRandomPlay() {
+	rand = Math.floor((Math.random() * 4) + 1);
+	machinePlay.push(rand);
+	return rand;
 }
 
-/*
-*	Function responsible for generating the next movement
-*/
-function generateMovement() {
+// Parse user input:
+function userMovement() {
 
-	printSeries(); 
+	// Game ends function:
+	if(!hasAWinner()){
+		// User correctly completed a round:
+		if ( userPlay.length === machinePlay.length 
+			&& userPlay[userCount] === machinePlay[userCount] ) {
+			userPlay = [];
+			userCount = 0;
 
-	if(!mistake)
-	{
-		playerTurn = false;
-		var newColor = Math.floor(Math.random() * 3);
-		switch(newColor) {
-			case 0:
-				currentColor = 'yellow';
-				$('#yellowSide').addClass('yellowSideGlow');
-				setTimeout(function() {$('#yellowSide').removeClass('yellowSideGlow');},1200);
-				break;
-			case 1:
-				currentColor = 'blue';
-				$('#blueSide').addClass('blueSideGlow');
-				setTimeout(function() {$('#blueSide').removeClass('blueSideGlow');},1200);
-				break;
-			case 2:
-				currentColor = 'red';
-				$('#redSide').addClass('redSideGlow');
-				setTimeout(function() {$('#redSide').removeClass('redSideGlow');},1200);
-				break;
-			case 3:
-				currentColor = 'green';
-				$('#greenSide').addClass('greenSideGlow');
-				setTimeout(function() {$('#greenSide').removeClass('greenSideGlow');},1200);
-				break;
+			setTimeout(function() {
+				counter++;
+				increaseScore();
+				turnOffLights();
+				check();
+			}, 500);
 		}
-		series.push(newColor); //add the new color to the list
-		playerTurn = true;
+
+		// User correctly identifies next element in series:
+		else if ( userPlay[userCount] === machinePlay[userCount] ) {
+			userCount++;
+			setTimeout(function() {
+				turnOffLights();
+			}, 500);
+		}
+
+		else if ( z > 0 ) {
+			machinePlay = [];
+			userPlay = [];
+			count = 0;
+			counter = 0;
+			n = 0;
+
+			losesGame();
+
+			// This is the restart function for strict mode:
+			setTimeout(function() {
+				cleanLostGame();
+				machineMovement();
+			}, 1750);
+		}
+
+		else {
+			n = 0;
+			losesGame();
+			setTimeout(function() {
+				cleanLostGame();
+				checkRepeat();
+
+			}, 750);
+
+		}
 	}
 }
 
-/*
-*	Function responsible for printing all the previous movements before generating
-*	the next one.
-*/
-function printSeries() {
-	for(var i = 0; i < series.length; i++) {
-		switch(series[i]) {
-			case 0:
-				$('#yellowSide').addClass('yellowSideGlow');
-				setTimeout(function() {$('#yellowSide').removeClass('yellowSideGlow');},1200);
-				break;
-			case 1:
-				$('#blueSide').addClass('blueSideGlow');
-				setTimeout(function() {$('#blueSide').removeClass('blueSideGlow');},1200);
-				break;
-			case 2:
-				$('#redSide').addClass('redSideGlow');
-				setTimeout(function() {$('#redSide').removeClass('redSideGlow');},1200);
-				break;
-			case 3:
-				$('#greenSide').addClass('greenSideGlow');
-				setTimeout(function() {$('#greenSide').removeClass('greenSideGlow');},1200);
-				break;
-		}
+// Repeat function for subsequent game rounds:
+function repeat() {
+
+	if ( machinePlay[n] === 1 ) {
+		greenLight();
+		n++;
 	}
+	else if ( machinePlay[n] === 2) {
+		redLight();
+		n++;
+	}
+	else if ( machinePlay[n] === 3) {
+		yellowLight();
+		n++;
+	}
+	else if (machinePlay[n] === 4) {
+		blueLight();
+		n++;
+	}	
+
 }
 
-/*
-*	Function responsible for capturing the player movement
-*/
-function playerMovement(elementID) {
-	if(elementID.indexOf(currentColor) !== -1) {
-		$('#' + elementID).addClass(elementID + 'Glow');
-		setTimeout(function() {$('#' + elementID).removeClass(elementID + 'Glow');},1200);
+// Check if the computer has iterated to the current round:
+function check() {
+
+	function clear() {
+		clearInterval(timeFunction);
+	}
+
+	var timeFunction = setInterval(function() {
+
+		turnOffLights();
+
+		if ( n !== machinePlay.length ) {
+			repeat();
+		}
+		else if ( n === machinePlay.length ) {
+			userPlay = [];
+			userCount = 0;
+			n = 0;
+			clear();
+			machineMovement();
+		}
+
+	}, 750);
+
+}
+
+// Check function for when the user fails a round (normal mode):
+function checkRepeat() {
+
+	function clear() {
+		clearInterval(timeFunction);
+	}
+
+	var timeFunction = setInterval(function() {
+		turnOffLights();
+
+		if ( n !== machinePlay.length ) {
+			repeat();
+		}
+		else if ( n === machinePlay.length ) {
+
+			userPlay = [];
+			userCount = 0;
+			n = 0;
+			clear();
+
+		}
+
+	}, 750);
+
+}
+
+function losesGame() {
+	$("#green").addClass("greenLoser");
+	$("#red").addClass("redLoser");
+	$("#yellow").addClass("yellowLoser");
+	$("#blue").addClass("blueLoser");
+}
+
+function cleanLostGame() {
+	$("#green").removeClass("greenLoser");
+	$("#red").removeClass("redLoser");
+	$("#yellow").removeClass("yellowLoser");
+	$("#blue").removeClass("blueLoser");
+
+	increaseScore();	
+}
+
+function hasAWinner() {
+	if ( userPlay[userCount] === machinePlay[userCount] 
+		&& machinePlay.length === 20 
+		&& userPlay.length === machinePlay.length) {
+
+		$("#green").addClass("greenWinner");
+		$("#red").addClass("redWinner");
+		$("#yellow").addClass("yellowWinner");
+		$("#blue").addClass("blueWinner");
+
+		$('#score').val('1');
+
 		return true;
 	}
-	else {
-		//sideError
-		$('#' + elementID).addClass('sideError');
-		setTimeout(function() {$('#' + elementID).removeClass('sideError');},500);
-		mistake = true;
-		return false;
-	}
+	return false;
 }
 
-
-/*
-*	Function responsible for starting the game in strict mode
-*/
-function startStrictMode() {
-	if($('#strictModeButton').hasClass('strictModeOn')){
-		$('#strictModeButton').removeClass();
-		mode = 'normal';
-	}
-	else {
-		$('#strictModeButton').addClass('strictModeOn');
-		mode = 'strict';
-	}
+function cleansCounters() {
+	n = 0;
+	machinePlay = [];
+	userPlay = [];
+	count = 0;
+	counter = 0;
+	userCount = 0;
 }
 
-/*
-*	Function responsible for starting the game
-*/
-function restartGame() {
-	$('#startButton').show();
+function increaseScore() {
+	var score = parseInt($('#score').val()) + 1;
+	$('#score').val(score);	
 }
