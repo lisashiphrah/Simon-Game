c16a019bdeb03ef6fcc5dc7aec59ad008585cf87
Cassia Almeida <lisa.shiphrah@gmail.com>
2017-04-18 09:28:32 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,247 +1,409 @@
-var side = '';
-var computerSide = '';
-var winner = '';
-var gameIsOver = false;
 
-$(document).ready(function(){
+/**
+ * Global variables
+ */
+ var machinePlay = [];
+var userPlay = [];
+var z = -1;
+var w = -1;
+var rand = 1;
+var count = 0;
+var counter = 0;
+var userCount = 0;
+var n = 0;
 
-	$('.sides').mouseenter(function(){
-		$(this).addClass('bounce');
-	});
+/**
+ * Sounds files
+ */
+var yellowAudio = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound3.mp3');
+var blueAudio = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound4.mp3');
+var redAudio = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound2.mp3');
+var greenAudio = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound1.mp3');
 
-	$('.sides').mouseleave(function(){
-		$(this).removeClass('bounce');
-	});
+
+/**
+ * Function called when user pressed the START button
+ */
+function startGame() {
+	cleansCounters();
+	$('#startButton').hide();
+	$('#score').val('1');
+	machineMovement();
+}
+
+/**
+ * Function called when user pressed the RESTART button
+ */
+function restartGame(){
+	$('#startButton').show();
+	$('#score').val('0');
+}
+
+// Reset function for all buttons:
+function turnOffLights() {
+	$("#green").removeClass("greenGlow");
+	$("#red").removeClass("redGlow");
+	$("#yellow").removeClass("yellowGlow");
+	$("#blue").removeClass("blueGlow");			
+}
+
+// Input switches control On/Off State and Strict Mode:
+$('#switchRight').change(function(){
+	z *= -1;
+});
+
+$('#switchLeft').change(function(){
+
+	w *= -1;
+
+	if ( w > 0 ) {
+		$("#startButton").css("color", "black");
+	}
+
+	if ( w < 0 ) {
+
+		n = 0;
+		machinePlay = [];
+		userPlay = [];
+		count = 0;
+		counter = 0;
+		userCount = 0;
+		turnOffLights();
+		$('#score').val('0');	
+
+		$("#startButton").css("color", "rgb(200,200,200");
+
+		$("#green").removeClass("greenLoser");
+		$("#red").removeClass("redLoser");
+		$("#yellow").removeClass("yellowLoser");
+		$("#blue").removeClass("blueLoser");
+
+		$("#green").removeClass("greenWinner");
+		$("#red").removeClass("redWinner");
+		$("#yellow").removeClass("yellowWinner");
+		$("#blue").removeClass("blueWinner");
+	}
 
 });
 
 /**
-* Function called after user selects a side to play
-* param {value} side selected
-*/
-function selectedSide(value){
-	side = value;
-	$('#initGameContainer').hide();
-	$('#boardContainer').show();
+ * Function called when AI makes a movement
+ */
+function machineMovement() {
+	var x = 1;
+	function clear() {
+		clearInterval(machineMovement);
+	}
 
-	initGame();
+	if (count > 0) {
+		x = 0;
+	}
+	var machineMovement = setInterval(function() {
+		turnOffLights();
+		generateRandomPlay();
+		
+		if (rand === 1) {
+			greenLight();
+		}
+		else if (rand === 2) {
+			redLight();
+		}
+		else if (rand === 3) {
+			yellowLight();	
+		}
+		else if (rand === 4) {
+			blueLight();		
+		}
+
+		count++;
+
+		// Resets buttons after machine plays final round, so the user can continue:
+		if (count === counter + 1) {
+			clear();
+			setTimeout(function() {
+				turnOffLights();
+			}, 500);
+		}
+
+	}, 750 * x); // Pause between iterations of button press functions
+
+};
+
+/**
+ * Generate a random number between 1 and 4 for each round:
+ */
+function generateRandomPlay() {
+	rand = Math.floor((Math.random() * 4) + 1);
+	machinePlay.push(rand);
+	return rand;
 }
 
 /**
-* Function responsible for starting the game
-*/
-function initGame() {
-	if(side !== 'X') { //If user selected O, computer starts playing
-		computerSide = 'X';
-		computerMovement();
-	}
-	else {
-		computerSide = 'O';
-	}
-}
+ * Function called when user makes a movement
+ */
+function userMovement() {
 
-/**
-* Function called when the user makes his movement
-* param {element} element/position clicked
-*/
-function makeMovement(element){
-	if(!gameIsOver){
-		if($(element).text() === ''){
-			$(element).text(side);
+	if(!hasAWinner()){
+		// User correctly completed the game
+		if (userPlay.length === machinePlay.length && userPlay[userCount] === machinePlay[userCount]) {
+			userPlay = [];
+			userCount = 0;
+
+			setTimeout(function() {
+				counter++;
+				increaseScore();
+				turnOffLights();
+				check();
+			}, 500);
 		}
-		gameIsOver = checkWinner();
-		if(!gameIsOver) { //if there is no winner, computer plays
-			setTimeout(function(){ computerMovement(); }, 300);
+
+		// User correctly identifies next element in series:
+		else if (userPlay[userCount] === machinePlay[userCount] ) {
+			userCount++;
+			setTimeout(function() {
+				turnOffLights();
+			}, 500);
 		}
-	}
-}
 
-/**
-* Computer play
-* Here there is a very basic AI implementation
-*/
-function computerMovement(){
-	if(	($('#11').text() === side ||
-		$('#21').text() === side ||
-		$('#23').text() === side ||
-		$('#31').text() === side ||
-		$('#33').text() === side)  &&
-		$('#22').text() === '') {
-		$('#22').text(computerSide);
-	}
-	else if(($('#11').text() === side || $('#13').text() === side) && $('#12').text() === ''){
-		$('#12').text(computerSide);
-	}
-	else if(($('#31').text() === side || $('#33').text() === side) && $('#32').text() === ''){
-		$('#32').text(computerSide);
-	}
-	else if(($('#31').text() === side || $('#32').text() === side) && $('#33').text() === ''){
-		$('#33').text(computerSide);
-	}
-	else if(($('#21').text() === side || $('#22').text() === side) && $('#23').text() === ''){
-		$('#23').text(computerSide);
-	}
-	else if(($('#12').text() === side|| $('#13').text() === side) && $('#11').text() === ''){
-		$('#11').text(computerSide);
-	}
-	else {
-		randomPlay();
-	}
-	setTimeout(function(){ gameIsOver = checkWinner(); }, 300);
-}
+		else if ( z > 0 ) {
+			machinePlay = [];
+			userPlay = [];
+			count = 0;
+			counter = 0;
+			n = 0;
 
-/**
-* Gets a random free position and play
-*/
-function randomPlay(){
-	var validPlay = false;
-	while(!validPlay){
-		var random = Math.random() * 10;
-		if(random < 1 && $('#11').text() === ''){
-			$('#11').text(computerSide);
-			validPlay = true;
+			losesGame();
+
+			// This is the restart function for strict mode:
+			setTimeout(function() {
+				cleanLostGame();
+				machineMovement();
+			}, 1750);
 		}
-		else if(random < 2 && $('#12').text() === ''){
-			$('#12').text(computerSide);
-			validPlay = true;
-		}
-		else if(random < 3 && $('#13').text() === ''){
-			$('#13').text(computerSide);
-			validPlay = true;
-		}
-		else if(random < 4 && $('#21').text() === ''){
-			$('#21').text(computerSide);
-			validPlay = true;
-		}
-		else if(random < 5 && $('#22').text() === ''){
-			$('#22').text(computerSide);
-			validPlay = true;
-		}
-		else if(random < 6 && $('#23').text() === ''){
-			$('#23').text(computerSide);
-			validPlay = true;
-		}
-		else if(random < 7 && $('#31').text() === ''){
-			$('#31').text(computerSide);
-			validPlay = true;
-		}
-		else if(random < 8 && $('#32').text() === ''){
-			$('#32').text(computerSide);
-			validPlay = true;
-		}
-		else if($('#33').text() === ''){
-			$('#33').text(computerSide);
-			validPlay = true;
-		}
-	}
-}
 
-/**
-* Function called when the button 'Restart Game' is clicked.
-* Restarts the game.
-*/
-function restartGame(){
-	winner = '';
-	gameIsOver = false;
-	$('#boardContainer').hide();
-	$('#initGameContainer').show();
-	cleanBoard();
-}
-
-/**
-* Function called to verify if the game is over and if there is a winner
-*/
-function checkWinner(){
-	var pos11 = $('#11').text();
-	var pos12 = $('#12').text();
-	var pos13 = $('#13').text();
-	var pos21 = $('#21').text();
-	var pos22 = $('#22').text();
-	var pos23 = $('#23').text();
-	var pos31 = $('#31').text();
-	var pos32 = $('#32').text();
-	var pos33 = $('#33').text();
-
-	//Same line
-	if(pos11 === pos12 && pos12 === pos13 && pos11 !== '') {
-		winner = pos11;
-	}
-
-	else if(pos21 === pos22 && pos22 === pos23 && pos21 !== '') {
-		winner = pos22;
-	}
-
-	else if(pos31 === pos32 && pos32 === pos33 && pos31 !== '') {
-		winner = pos31;
-	}
-
-	//Same column
-	else if(pos11 === pos21 && pos21 === pos31 && pos11 !== '') {
-		winner = pos11;
-	}
-
-	else if(pos12 === pos22 && pos22 === pos32 && pos12 !== '') {
-		winner = pos22;
-	}
-
-	else if(pos13 === pos23 && pos23 === pos33 && pos13 !== '') {
-		winner = pos13;
-	}
-
-	//Diagonal
-	else if(pos11 === pos22 && pos22 === pos33 && pos11 !== '') {
-		winner = pos22;
-	}
-
-	else if(pos13 === pos22 && pos22 === pos31 && pos13 !== '') {
-		winner = pos22;
-	}
-	else {
-		if(pos11 !== '' && pos12 !== '' && pos13 !== '' && pos21 !== '' && pos22 !== '' &&
-			pos23 !== '' && pos31 !== '' && pos32 !== '' && pos33 !== ''){
-				winner = 'DRAW';
-		}
-	}
-
-	if(winner !== ''){
-		if(winner === 'DRAW') {
-			setTimeout(function(){ alert('DRAW'); }, 100);
-		}
 		else {
-			setTimeout(function(){ alert('winner: ' + winner); }, 100);
+			n = 0;
+			losesGame();
+			setTimeout(function() {
+				cleanLostGame();
+				checkRepeat();
+			}, 750);
 		}
-		switch(winner){
-			case 'X':
-				var scoreX = parseInt($('#xScore').text(), 10)  + 1;
-				$('#xScore').text('' + scoreX);
-				break;
-			case 'O':
-				var scoreO = parseInt($('#oScore').text(), 10)  + 1;
-				$('#oScore').text('' + scoreO);
-				break;
-			case 'DRAW':
-				var scoreXDraw = parseInt($('#xScore').text(), 10) + 1;
-				$('#xScore').text('' + scoreXDraw);
-				var scoreODraw = parseInt($('#oScore').text(), 10)  + 1;
-				$('#oScore').text('' + scoreODraw);
-				break;
+	}
+}
+
+
+/**
+ * Prints the sequence generated until the moment
+ */
+function printSequence() {
+
+	if (machinePlay[n] === 1 ) {
+		greenLight();
+		n++;
+	}
+	else if (machinePlay[n] === 2) {
+		redLight();
+		n++;
+	}
+	else if (machinePlay[n] === 3) {
+		yellowLight();
+		n++;
+	}
+	else if (machinePlay[n] === 4) {
+		blueLight();
+		n++;
+	}	
+}
+
+/**
+ * Check if the computer has iterated to the current round:
+ */
+function check() {
+
+	function clear() {
+		clearInterval(timeFunction);
+	}
+
+	var timeFunction = setInterval(function() {
+
+		turnOffLights();
+
+		if ( n !== machinePlay.length ) {
+			printSequence();
 		}
+		else if ( n === machinePlay.length ) {
+			userPlay = [];
+			userCount = 0;
+			n = 0;
+			clear();
+			machineMovement();
+		}
+
+	}, 750);
+
+}
+
+/**
+ * Check function for when the user fails a round (normal mode):
+ */
+function checkRepeat() {
+
+	function clear() {
+		clearInterval(timeFunction);
+	}
+
+	var timeFunction = setInterval(function() {
+		turnOffLights();
+
+		if ( n !== machinePlay.length ) {
+			printSequence();
+		}
+		else if ( n === machinePlay.length ) {
+
+			userPlay = [];
+			userCount = 0;
+			n = 0;
+			clear();
+		}
+	}, 750);
+}
+
+/**
+ * Adds classes and styles to the game when user loses
+ */
+function losesGame() {
+	$("#green").addClass("greenLoser");
+	$("#red").addClass("redLoser");
+	$("#yellow").addClass("yellowLoser");
+	$("#blue").addClass("blueLoser");
+}
+
+/**
+ * Cleans classes added when user lost the game
+ */
+function cleanLostGame() {
+	$("#green").removeClass("greenLoser");
+	$("#red").removeClass("redLoser");
+	$("#yellow").removeClass("yellowLoser");
+	$("#blue").removeClass("blueLoser");
+
+	increaseScore();	
+}
+
+/**
+ * Function responsible for verifying if there's a winner
+ * @return true if there is a winner
+ */
+function hasAWinner() {
+	if (userPlay[userCount] === machinePlay[userCount] 
+		&& machinePlay.length === 20 
+		&& userPlay.length === machinePlay.length) {
+
+		$("#green").addClass("greenWinner");
+		$("#red").addClass("redWinner");
+		$("#yellow").addClass("yellowWinner");
+		$("#blue").addClass("blueWinner");
+
+		$('#score').val('1');
+
 		return true;
 	}
 	return false;
 }
 
 /**
-* Cleans the board for the next game
-*/
-function cleanBoard() {
-	var pos11 = $('#11').text('');
-	var pos12 = $('#12').text('');
-	var pos13 = $('#13').text('');
-	var pos21 = $('#21').text('');
-	var pos22 = $('#22').text('');
-	var pos23 = $('#23').text('');
-	var pos31 = $('#31').text('');
-	var pos32 = $('#32').text('');
-	var pos33 = $('#33').text('');
+ * Cleans global variables and counter
+ */
+function cleansCounters() {
+	n = 0;
+	machinePlay = [];
+	userPlay = [];
+	count = 0;
+	counter = 0;
+	userCount = 0;
+}
+
+/**
+ * Function responsible for increasing the score
+ */
+function increaseScore() {
+	var score = parseInt($('#score').val()) + 1;
+	$('#score').val(score);	
+}
+
+//////////////////// YELLOW BUTTON EVENTS //////////////////////
+/**
+ * Function when Yellow button is pressed
+ */
+function yellowClick() {
+	yellowLight();
+	userPlay.push(3);
+	userMovement();	
+}
+/**
+ * Turns on yellow lights and play sound
+ */
+function yellowLight() {
+	turnOffLights()	
+	$('#yellow').toggleClass("yellowGlow");
+	yellowAudio.play();
+}
+
+
+//////////////////// BLUE BUTTON EVENTS //////////////////////
+
+/**
+ * Function when Blue button is pressed
+ */
+function blueClick() {
+	blueLight();
+	userPlay.push(4);
+	userMovement();	
+}
+/**
+ * Turns on blue lights and play sound
+ */
+function blueLight() {
+	turnOffLights()
+	$('#blue').toggleClass("blueGlow");
+	blueAudio.play();
+}
+
+//////////////////// RED BUTTON EVENTS //////////////////////
+
+/**
+ * Function when Red button is pressed
+ */
+function redClick() {
+	redLight();
+	userPlay.push(2);
+	userMovement();	
+}
+/**
+ * Turns on red lights and play sound
+ */
+function redLight() {
+	turnOffLights()
+	$('#red').toggleClass("redGlow");
+	redAudio.play();
+}
+
+
+//////////////////// GREEN BUTTON EVENTS //////////////////////
+/**
+ * Function when Green button is pressed
+ */
+function greenClick(){
+	greenLight();
+	userPlay.push(1);
+	userMovement();
+}
+/**
+ * Turns on green lights and play sound
+ */
+function greenLight() {
+	turnOffLights();
+	$('#green').toggleClass("greenGlow");
+	greenAudio.play();
 }
